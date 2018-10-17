89484c6c765b2e311e31ddf1bab6e59de1ecd8f3
tueseed <tue_seed@hotmail.com>
2018-10-17 21:00:21 +0700

    Add files via upload

--- firstbot.php
+++ firstbot.php
@@ -32,7 +32,7 @@ if (!is_null($events['events'])) //check ค่าในตัวแปร $even
             $replyToken = $event['replyToken']; //เก็บ reply token เอาไว้ตอบกลับ
             $source_type = $event['source']['type'];//เก็บที่มาของ event(user หรือ group)
             $txtin = $event['message']['text'];//เอาข้อความจากไลน์ใส่ตัวแปร $txtin
-            reply_msg($txtin,replyToken);      
+            reply_msg($txtin,$replyToken);      
         }
     }
 }

b41926ea15177ecf6f899b4fe18ffdaa5167e4a0
tueseed <tue_seed@hotmail.com>
2019-03-17 19:24:15 +0700

    Add files via upload

--- firstbot_push.php
+++ firstbot_push.php
@@ -3,7 +3,7 @@
 	{
 			$json1 = '{
 						"type":"flex",
-						"altText":"แจ้งเตือนการประชุม",
+						"altText":"Green Office",
 						"contents":{
 									"type": "bubble",
 									"header": {
@@ -12,7 +12,7 @@
 												"contents": [
 																{
 																	"type": "text",
-																	"text": "THE ASSISTANT",
+																	"text": "ประชาสัมพันธ์",
 																	"weight": "bold",
 																	"color": "#5b2a72",
 																	"align": "center",
@@ -27,115 +27,6 @@
 												"aspectRatio": "20:13",
 												"aspectMode": "cover"
 											},
-									"body": {
-												"type": "box",
-												"layout": "vertical",
-												"spacing": "sm",
-												"contents": [
-															 {
-																"type": "text",
-																"text": "แจ้งเตือนการประชุม",
-																"size": "xl",
-																"align": "center",
-																"weight": "bold"
-															},
-															{
-																"type": "box",
-																"layout": "vertical",
-																"spacing": "sm",
-																"contents": [
-																				{
-																					"type": "box",
-																					"layout": "baseline",
-																					"contents": [
-																									{
-																										"type": "icon",
-																										"url": "https://meetingnotify.herokuapp.com/pic/content.png"
-																									},
-																									{
-																										"type": "text",
-																										"text": "เรื่องอะไร",
-																										"wrap": true,																	
-																										"margin": "sm",
-																										"flex": 0
-																									}
-																								]
-																				},
-																				{
-																					"type": "box",
-																					"layout": "baseline",
-																					"contents": [
-																									{
-																										"type": "icon",
-																										"url": "https://meetingnotify.herokuapp.com/pic/calendar.png"
-																									},
-																									{
-																										"type": "text",
-																										"text": "วันที่ รอคอย",																								
-																										"margin": "sm",
-																										"flex": 0
-																									}
-																								]
-																				},
-																				{
-																					"type": "box",
-																					"layout": "baseline",
-																					"contents": [
-																									{
-																										"type": "icon",
-																										"url": "https://meetingnotify.herokuapp.com/pic/alarm-clock.png"
-																									},
-																									{
-																										"type": "text",
-																										"text":"เวลา",																								
-																										"margin": "sm",
-																										"flex": 0
-																									}
-																								]
-																				},
-																				{
-																					"type": "box",
-																					"layout": "baseline",
-																					"contents": [
-																									{
-																										"type": "icon",
-																										"url": "https://meetingnotify.herokuapp.com/pic/school.png"
-																									},
-																									{
-																										"type": "text",
-																										"text":"สถานที่",																								
-																										"margin": "sm",
-																										"flex": 0
-																									}
-																								]
-																				},
-																				{
-																					"type": "box",
-																					"layout": "baseline",
-																					"contents": [
-																									{
-																										"type": "icon",
-																										"url": "https://meetingnotify.herokuapp.com/pic/meeting.png"
-																									},
-																									{
-																										"type": "text",
-																										"text": "ผู้เข้าประชุม",																								
-																										"margin": "sm",
-																										"flex": 0
-																									}
-																								]
-																				}
-																			]
-															},
-															{
-																"type": "text",
-																"text": "555+",
-																"wrap": true,
-																"color": "#aaaaaa",
-																"size": "xxs"
-															}
-															]
-										},
 									"footer": {
 												"type": "box",
 												"layout": "vertical",

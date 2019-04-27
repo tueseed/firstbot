33da9da72e85914fb80e8269168f42da63006c68
Nutthapong <tue_seed@hotmail.com>
2019-04-27 18:14:54 +0700

    wqw

--- pukpuy/ai3.php
+++ pukpuy/ai3.php
@@ -1,22 +1,21 @@
 <?php
-function response($query_text)
+function response()
 {
   $json = '{
-              "speech": '.$query_text.'",
+              "speech": "this text is spoken out loud if the platform supports voice interactions",
               "displayText": "this text is displayed visually"
             }';
-
   $j_return = json_decode($json,true);
   header("Content-Type: application/json");
+  //echo $j_return;
 	echo json_encode($j_return);
 }
 header("Content-Type: application/json");	
 $method = $_SERVER["REQUEST_METHOD"];
 if($method == "POST")
 {
-  $requestBody = file_get_contents('php://input');
-  $data = json_decode($requestBody,true);
-	response($data);
+	$requestBody = file_get_contents('php://input');
+	response();
 }else{
 	echo "Method Not allow";
 }

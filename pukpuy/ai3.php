56143d97790545157258f37b12e0eee6e40ddb4d
Nutthapong <tue_seed@hotmail.com>
2019-04-27 18:12:00 +0700

    weqewqe

--- pukpuy/ai3.php
+++ pukpuy/ai3.php
@@ -1,36 +1,22 @@
 <?php
-function response()
+function response($query_text)
 {
-  $response = new stdclass();
-  $fulfillmentMessages = array();
-	$speech = array(
-		   'simpleResponse' => array(
-			   'textToSpeech' => 'HI THIS ...',
-			   'displayText' => 'HI THIS .....'
-			)
-    );
-    $text_message = array('messages'=>array('speech'=>'hihi','type'=>0));
-	$response = array(
-		   'fulfillmentText' => '123456789',	
-			'fulfillmentMessages' => array(
-                                      'messages'=> array('speech'=>'Text response','type'=>0)
-      )		   
-     );
   $json = '{
-              "speech": "this text is spoken out loud if the platform supports voice interactions",
+              "speech": '.$query_text.'",
               "displayText": "this text is displayed visually"
             }';
+
   $j_return = json_decode($json,true);
   header("Content-Type: application/json");
-  //echo $j_return;
 	echo json_encode($j_return);
 }
 header("Content-Type: application/json");	
 $method = $_SERVER["REQUEST_METHOD"];
 if($method == "POST")
 {
-	$requestBody = file_get_contents('php://input');
-	response();
+  $requestBody = file_get_contents('php://input');
+  $data = json_decode($requestBody,true);
+	response($data);
 }else{
 	echo "Method Not allow";
 }

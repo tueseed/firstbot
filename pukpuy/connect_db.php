d35696dd75655680d24eb25556b1e060ef6625d5
Nutthapong <tue_seed@hotmail.com>
2019-04-28 19:14:12 +0700

    ddfdsf

--- pukpuy/connect_db.php
+++ pukpuy/connect_db.php
@@ -1,7 +1,7 @@
 <?php
     $host = 'us-cdbr-iron-east-01.cleardb.net';
     $user = 'b859a12ba0c2d9';
-    $pass = 'e2e15cf';
+    $pass = 'e2e15cf7';
     $db = 'heroku_700de64fb6a62d3';
     $conn = new mysqli($host,$user,$pass,$db);
     if ($conn->connect_error) 

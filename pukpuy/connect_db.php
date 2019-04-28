e735781e5d07a260a20cb84d98de925849ccf5fd
Nutthapong <tue_seed@hotmail.com>
2019-04-28 19:42:40 +0700

    dsadsad

--- pukpuy/connect_db.php
+++ pukpuy/connect_db.php
@@ -4,10 +4,5 @@
     $pass = 'e2e15cf7';
     $db = 'heroku_700de64fb6a62d3';
     $conn = new mysqli($host,$user,$pass,$db);
-    if ($conn->connect_error) 
-    {
-        die("Connection failed: " . $conn->connect_error);
-    }
     mysqli_query($conn, "SET NAMES utf8");
-    echo 'Mysqli Connected....';
 ?>

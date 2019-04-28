012fbde677fb4408e16db1c37a92dfec96e4fe7d
Nutthapong <tue_seed@hotmail.com>
2019-04-28 19:11:25 +0700

    sdadasd

---
+++ pukpuy/connnect_db.php
@@ -0,0 +1,12 @@
+<?php
+    $host = 'us-cdbr-iron-east-01.cleardb.net';
+    $user = 'b859a12ba0c2d9';
+    $pass = 'e2e15cf';
+    $db = 'heroku_700de64fb6a62d3';
+    $conn = new mysqli($host,$user,$pass,$db);
+    if ($conn->connect_error) 
+    {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    mysqli_query($conn, "SET NAMES utf8");
+?>

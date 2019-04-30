ffb8cffecfb485542b6d513b0fa188b0f4100bed
Nutthapong <tue_seed@hotmail.com>
2019-04-30 09:10:16 +0700

    sadasdsa

--- pukpuy/api/query.php
+++ pukpuy/api/query.php
@@ -1,5 +1,10 @@
 <?php
+    require('connect_db.php');
+
     $office = $_POST["office"];
     $keyword = $_POST["keyword"];
-    echo $office.$keyword;
+    $sql_text = "SELECT * FROM tbl_equipment WHERE office_id = $office AND (equip_id LIKE '%$keyword%' OR equip_name LIKE '%$keyword%')";
+    $query = mysqli_query($conn,$sql_text);
+    $data_return = mysqli_fetch_assoc($query);
+    echo json_encode($data_return);
 ?>
